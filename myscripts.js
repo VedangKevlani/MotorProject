0a2cd1d1e1c7edbfe767b671663c63b24a34e3d2
VedangKevlani <kevlanivedang28@gmail.com>
2024-01-09 14:19:10 -0500

    added welcome alert to home page

---
+++ myscripts.js
@@ -0,0 +1,2 @@
+alert("Welcome to DriveToJamaica! Use the search bar by typing: I want to... followed by your request.");
+alert("Optionally, you may click on the menu buttons to use the site features.");
